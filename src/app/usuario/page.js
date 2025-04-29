4e9b37054563e8ef536de00a193d2c4b43a49460
Eduarda Balbino <eduarda.balbino@academico.ifpb.edu.br>
2025-04-29 09:31:39 -0300

    Mudanças de texto da página

--- src/app/usuario/page.js
+++ src/app/usuario/page.js
@@ -1,6 +1,18 @@
-export default () => (
-    <>
-    <h1>Loja de Roupas e Cosméticos</h1>
-    <p>Promoções</p>
-    </>
-)
+import db from "@/lib/db"
+export default async () => {
+    const usuario = await db.query("select * from usuario")
+ return (<>
+    <h1>Lista de alunos</h1>
+    <div>
+      {
+         usuario.rows.map( 
+            a => (
+               <div>
+                  <h2>{a.name}</h2>
+               </div>
+            ) 
+         )
+      }
+   </div>
+ </>);
+}
