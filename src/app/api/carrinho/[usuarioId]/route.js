d748ea52ff7a12a1264724c9914d5dbf3cca8480
Eduarda Balbino <eduarda.balbino@academico.ifpb.edu.br>
2025-10-14 11:33:42 -0300

    perfumes e cores adicionadas

--- src/app/api/carrinho/[usuarioId]/route.js
+++ src/app/api/carrinho/[usuarioId]/route.js
@@ -3,13 +3,13 @@ import pool from "@/lib/db";
 
 export async function GET(request, {params}) {
     try {
-        const {usuarioId}= await params;
+        const {cadastroId}= params;
 
         const client = await pool.connect( )
         const result = await client.query(
-        `SELECT p.nome, c.quantidade from item_carrinho i join produtos p ON (p.id = i.id_produto) WHERE i.usuario_id = $1`
-        [usuarioId]
-          )
+        `SELECT p.nome, c.quantidade from item_carrinho i join produtos p ON (p.id = i.id_produto) WHERE i.cadastro_id = $1`
+        [cadastroId]
+          );
           client.release()
           return NextResponse.json(result.rows)
         } catch(error) {
@@ -21,20 +21,20 @@ export async function GET(request, {params}) {
 export async function POST(request) {
     let client 
     try {
-        const { nome, preco, quantidade, cor} = await request.json()
+        const { id_cadastro, id_produtos, quantidade, data_adicao} = await request.json()
 
-        if(!usuarioId) {
+        if(!id_cadastro) {
             return NextResponse.json(
-                {error: "usuario_id é obrigatório"},
+                {error: "cadastro_id é obrigatório"},
                 {status: 400}
             )
         }
         client = await pool.connect()
-        await client.query(
-            `INSERT INTO adicionarCarrinho (produtos_id)
-            VALUES $1 `, 
-            [produtos_id]
-        )
+       await client.query(
+        `INSERT INTO item_carrinho (id_produto, cadastro_id, quantidade, data_adicao)
+         VALUES ($1, $2, $3, $4)`, 
+        [id_produtos, id_cadastro, quantidade, data_adicao]
+);  
         return NextResponse.json({message: "Produto adicionado com sucesso "}, {status: 201} )
     } catch (error) {
         console.error("Erro ao adicionar ao carrinho", error)
